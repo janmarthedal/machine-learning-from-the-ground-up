22c9df75b110ba7ddb94f50fe89d2c11a8ad2155
Jan Marthedal Rasmussen <jan@janmr.com>
2023-01-20 08:52:40 +0100

    Some variable renaming

--- simple_neural_network.py
+++ simple_neural_network.py
@@ -2,24 +2,24 @@ import numpy as np
 from collections import namedtuple
 
 # Compute error/loss function
-def compute_error(a, y):
-    m = y.shape[1]   # number of training examples
-    return 0.5 * np.linalg.norm(a - y, 'fro') ** 2 / m
+def compute_error(A, Y):
+    m = Y.shape[1]   # number of training examples
+    return 0.5 * np.linalg.norm(A - Y, 'fro') ** 2 / m
 
-# Activation functions
-def sigmoid(z):
-    return 1.0 / (1.0 + np.exp(-z))
+def sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
 
-def sigmoid_prime(z):
-    t = sigmoid(z)
+def sigmoid_prime(x):
+    t = sigmoid(x)
     return t * (1 - t)
 
+# Activation functions and their derivatives
 SIGMOID_ACTIVATION = (sigmoid, sigmoid_prime)
-IDENTITY_ACTIVATION = (lambda z: z, lambda z: np.ones(z.shape))
+IDENTITY_ACTIVATION = (lambda x: x, lambda x: np.ones(x.shape))
 RELU_ACTIVATION = (lambda x: (x >= 0) * x, lambda x: x >= 0)
 
-# Helper class to store values of z and a
-NodeValues = namedtuple('NodeValues', ['z', 'a'])
+# Helper class to store values of Z and A
+NodeValues = namedtuple('NodeValues', ['Z', 'A'])
 
 class Layer:
     def __init__(self, input_count, output_count, activation):
@@ -37,62 +37,62 @@ class SimpleNeuralNetwork:
         self.layers = [Layer(m, n, g) for m, n, g in zip(unit_counts[:-1], unit_counts[1:], activations)]
 
     # Compute output of each layer
-    def evaluate(self, a):
-        values = [NodeValues(None, a)]
+    def evaluate(self, A):
+        values = [NodeValues(None, A)]
         for layer in self.layers:
-            z = np.dot(layer.W, a) + layer.b
-            a = layer.g[0](z)
-            values.append(NodeValues(z, a))
+            Z = np.dot(layer.W, A) + layer.b
+            A = layer.g[0](Z)
+            values.append(NodeValues(Z, A))
         return values
 
     # Compute gradient using back propagation
-    def compute_gradient(self, values, y):
-        m = y.shape[1]
+    def compute_gradient(self, values, Y):
+        m = Y.shape[1]
         L = len(self.layers)
-        dJdWs = []
-        dJdbs = []
+        dWs = []
+        dbs = []
 
         # l = L, L - 1, ..., 1
         for l in range(L, 0, -1):
             if l == L:
                 # special case for output layer
-                # dJda^l = (a^L - y) / m
-                dJda = (values[L].a - y) / m
+                # dA^l = (A^L - Y) / m
+                dA = (values[L].A - Y) / m
             else:
-                # dJda^l = W^(l+1)^T * dJdz^(l+1)
-                dJda = np.dot(self.layers[l + 1 - 1].W.T, dJdz)
+                # dA^l = W^(l+1)^T * dZ^(l+1)
+                dA = np.dot(self.layers[l + 1 - 1].W.T, dZ)
 
             # numpy '*' does element-wise multiplication
-            # dJdz^l = dJda^l * g^l'(z^l)
-            dJdz = dJda * self.layers[l - 1].g[1](values[l].z)
+            # dZ^l = dA^l * g^l'(Z^l)
+            dZ = dA * self.layers[l - 1].g[1](values[l].Z)
 
-            # dJdW^l = dJdz^l * a^(l-1)^T
-            dJdW = np.dot(dJdz, values[l - 1].a.T)
-            # dJdb^l = dJdz^l * [1 1 ... 1]^T
-            dJdb = np.sum(dJdz, axis=1, keepdims=True)
+            # dW^l = dZ^l * A^(l-1)^T
+            dW = np.dot(dZ, values[l - 1].A.T)
+            # db^l = dZ^l * [1 1 ... 1]^T
+            db = np.sum(dZ, axis=1, keepdims=True)
 
-            dJdWs.append(dJdW)
-            dJdbs.append(dJdb)
+            dWs.append(dW)
+            dbs.append(db)
 
-        dJdWs.reverse()
-        dJdbs.reverse()
+        dWs.reverse()
+        dbs.reverse()
 
-        return dJdWs, dJdbs
+        return dWs, dbs
 
     # Train the neural network
-    def train(self, xs, ys, epochs, learning_rate, callback):
-        values = self.evaluate(xs)
-        error = compute_error(values[-1].a, ys)
+    def train(self, Xs, Ys, epochs, learning_rate, callback):
+        values = self.evaluate(Xs)
+        error = compute_error(values[-1].A, Ys)
         callback(0, error)
         for epoch in range(epochs):
-            dJdWs, dJdbs = self.compute_gradient(values, ys)
-            for layer, dJdW, dJdb in zip(self.layers, dJdWs, dJdbs):
-                layer.W -= learning_rate * dJdW
-                layer.b -= learning_rate * dJdb
-            values = self.evaluate(xs)
-            error = compute_error(values[-1].a, ys)
+            dWs, dbs = self.compute_gradient(values, Ys)
+            for layer, dW, db in zip(self.layers, dWs, dbs):
+                layer.W -= learning_rate * dW
+                layer.b -= learning_rate * db
+            values = self.evaluate(Xs)
+            error = compute_error(values[-1].A, Ys)
             callback(epoch + 1, error)
-        return values[-1].a
+        return values[-1].A
 
 def iteration_hook(epoch, error):
     if epoch % 100 == 0:
@@ -103,8 +103,8 @@ if __name__ == '__main__':
     # produce the same initial weights every time
     np.random.seed(seed=0)
     nn = SimpleNeuralNetwork(1, [(20, SIGMOID_ACTIVATION), (1, IDENTITY_ACTIVATION)])
-    xs = np.arange(-5, 5, 0.1).reshape(1, -1)
-    ys = np.sin(xs)
-    output = nn.train(xs, ys, 2000, 0.1, iteration_hook)
-    plt.plot(xs.ravel(), ys.ravel(), 'b', xs.ravel(), output.ravel(), 'r')
+    Xs = np.arange(-5, 5, 0.1).reshape(1, -1)
+    Ys = np.sin(Xs)
+    output = nn.train(Xs, Ys, 2000, 0.1, iteration_hook)
+    plt.plot(Xs.ravel(), Ys.ravel(), 'b', Xs.ravel(), output.ravel(), 'r')
     plt.show()
